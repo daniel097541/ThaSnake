33776d816b8969266ed317578ea1d36ce9262a5d
Diego <diegojaraoro@gmail.com>
2018-06-06 20:13:08 +0200

    Added login and fixed table

--- ThaSnake/src/main/java/Client/Client.java
+++ ThaSnake/src/main/java/Client/Client.java
@@ -9,6 +9,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 import java.net.Socket;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class Client implements iSnake {
@@ -18,16 +19,44 @@ public class Client implements iSnake {
     private static ClientView view;
     private static ClientTable table;
     private static ClientObservable observable;
+    private static String ip;
+    private static String port;
 
     public static void main(String[] args) throws InterruptedException {
 
 
         //crear aqui el panel del login y sacar la ip de ahi
-        login();
-        view = new ClientView(handler);
-        observable = new ClientObservable(view);
-        table = new ClientTable();
+        
+        start();
 
+
+    }
+    
+    public static void start(){
+        boolean correctIp = false;
+        while(!correctIp){
+            ip = JOptionPane.showInputDialog("Introduzca la IP del servidor: ");
+            if(ip.equals(HOST)){
+                correctIp = true;
+                break;
+            }
+            JOptionPane.showMessageDialog(null, "IP incorrecta, introduzca una nueva");
+        }
+        boolean correctPort = false;
+        while(!correctPort){
+            port = JOptionPane.showInputDialog("Introduzca el puerto al que se quiere conectar: ");
+            int p = Integer.parseInt(port);
+            if(p == PORT){
+                correctPort = true;
+                login();
+                System.out.println();
+                view = new ClientView(handler);
+                observable = new ClientObservable(view);
+                table = new ClientTable();
+                break;
+            }
+            JOptionPane.showMessageDialog(null,"Puerto incorrecto, introduzca uno nuevo");
+        }
     }
 
     public static void paint(int x, int y, String color){
@@ -40,14 +69,14 @@ public class Client implements iSnake {
     }
     
     public static void modifyRow(Integer i,Object[] row,Integer points){
-        table.getjTable1().getModel().setValueAt(points,i,0);
+        table.getjTable1().getModel().setValueAt(points,i,1);
     }
-    
+   
     public static void modifyTable(Integer id,Integer points){
         Object row[] = {id,points};
         boolean found = false;
         for(int i=0;i<table.getjTable1().getRowCount()&&!found;i++){
-            Integer valor = (Integer) table.getjTable1().getValueAt(i,1);
+            Integer valor = (Integer) table.getjTable1().getValueAt(i,0);
             if(valor == id){
                 modifyRow(i,row,points);
                 found = true;
@@ -61,18 +90,12 @@ public class Client implements iSnake {
 
     private static void login(){
         try {
-            Socket socket = new Socket(HOST,PORT);
+            Socket socket = new Socket(ip,PORT);
             handler = new ClientHandler(socket,new DataInputStream(socket.getInputStream()), new DataOutputStream(socket.getOutputStream()));
             System.out.println("Logueado en el servidor!");
         } catch (Exception e) {
             System.out.println("Error: No se ha podido logear en el servidor!");
         }
     }
-
-
-
-
-
-
 
 }
